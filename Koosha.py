282c379a4fed45f68f512c9298ff2b847417573d
Ali <mansooriha440@gmail.com>
2022-07-08 16:19:42 +0430

    Changed by Ali

--- Koosha.py
+++ Koosha.py
@@ -3,8 +3,9 @@ from getpass import getpass
 username = input("Enter Your Username: ")
 password = getpass("Enter Your Password: ")
 
-if username == "akbar" and password == "maktab78":
+if username == "Koosha" and password == "maktab78":
     print("Login Successfully.")
 else:
     print("Username or Password Wrong.")
 
+print('Changed by Ali')
